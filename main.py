ef85fd1be7ca7c0a60950e3c7246da1b051c55ed
un-lock-able <gaogao174561@outlook.com>
2021-09-12 20:43:18 +0800

    Modified Outputbar so that it can output the signal in different format.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def make_wire(start_gate, start_port_num, end_gate, end_port_num):
 
 
 def main():
+    '''
     input_bar = gate.UserInputBar(2)
     input_bar.set_content(0, True)
     input_bar.set_content(1, False)
@@ -29,6 +30,14 @@ def main():
     wires.append(make_wire(or_gate1, 0, and_gate, 0))
     wires.append(make_wire(or_gate2, 0, and_gate, 1))
     wires.append(make_wire(and_gate, 0, output_bar, 0))
+    input_bar.start_sending()
+    '''
+    input_bar=gate.UserInputBar(2)
+    output_bar=gate.UserOutputBar(2, print_out=True, print_format=1)
+    wires = []
+    wires.append(make_wire(input_bar, 0, output_bar, 0))
+    wires.append(make_wire(input_bar, 1, output_bar, 1))
+    input_bar.set_content(0, True)
     input_bar.start_sending()
 
 
